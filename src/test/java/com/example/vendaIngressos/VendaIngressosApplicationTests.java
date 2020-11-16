ac2c528a9630e0dcc54a6a57a79ec4cf1f1cdce5
matheus <matheusbvieira@hotmail.com>
2020-11-16 19:54:26 -0300

    Commit inicial

---
+++ src/test/java/com/example/vendaIngressos/VendaIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vendaIngressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
