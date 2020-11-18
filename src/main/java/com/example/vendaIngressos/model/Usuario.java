3390bef5f75c9f0129508b8ef9a101eac6c55037
matheus <matheusbvieira@hotmail.com>
2020-11-17 21:27:09 -0300

    feat: Autenticação completa

--- src/main/java/com/example/vendaIngressos/model/Usuario.java
+++ src/main/java/com/example/vendaIngressos/model/Usuario.java
@@ -1,17 +1,31 @@
 package com.example.vendaIngressos.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,6 +46,9 @@ public class Usuario {
 	public String email;
 	public String telefone;
 	public int idade;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfis = new ArrayList<>();
 
 	public Usuario(String nomeCompleto, String cpf, Endereco endereco, String username, String password, String email,
 			String telefone, int idade) {
@@ -81,6 +98,7 @@ public class Usuario {
 		this.endereco = endereco;
 	}
 
+	@Override
 	public String getUsername() {
 		return username;
 	}
@@ -89,6 +107,7 @@ public class Usuario {
 		this.username = username;
 	}
 
+	@Override
 	public String getPassword() {
 		return password;
 	}
@@ -119,6 +138,31 @@ public class Usuario {
 
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
--- src/main/java/com/example/vendaIngressos/repository/UsuarioRepository.java
+++ src/main/java/com/example/vendaIngressos/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.example.vendaIngressos.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.vendaIngressos.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	Optional<Usuario> findByEmail(String username);
 
 }
