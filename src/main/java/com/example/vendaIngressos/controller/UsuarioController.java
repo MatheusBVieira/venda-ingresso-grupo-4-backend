93cc8dbfe1a9019c4d77845ec4cd2dc840dd0973
Victor Ohashi <victor.ohashi@softplan.com.br>
2020-11-27 20:24:39 -0300

    Removed unused imports

--- src/main/java/com/example/vendaIngressos/controller/UsuarioController.java
+++ src/main/java/com/example/vendaIngressos/controller/UsuarioController.java
@@ -1,7 +1,5 @@
 package com.example.vendaIngressos.controller;
 
-import java.net.URI;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
