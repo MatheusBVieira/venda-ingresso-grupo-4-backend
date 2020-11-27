0c65fc7389f4ce0b4d36516f201dea55db203eae
Victor Ohashi <victor.ohashi@softplan.com.br>
2020-11-27 18:18:44 -0300

    Fix returned status

--- src/main/java/com/example/vendaIngressos/controller/AutenticacaoController.java
+++ src/main/java/com/example/vendaIngressos/controller/AutenticacaoController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -51,7 +52,7 @@ public class AutenticacaoController {
 		if (tokenService.isTokenValido(token)) {
 			return ResponseEntity.ok().build();
 		}
-		return ResponseEntity.badRequest().build();
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 
 	}
 
